e662d212a743eb9ac45e956d743a81f65bdda7c9
Kristian Hartikainen <kristian@statwing.com>
2015-06-20 15:20:01 +0300

    Imports and initial variables for Compiler.atg. Added Compiler.java, which is pretty much exact copy of Hirvola's example project's Compiler.java [http://www.niksula.hut.fi/~hirvolt1/ExampleProject.tar.gz].

---
+++ Compiler.java
@@ -0,0 +1,40 @@
+import fi.tkk.cs.tkkcc.SlxCompiler;
+import fi.tkk.cs.tkkcc.slx.*;
+
+// import mycompiler.SymbolTable;
+
+// import Parser;
+// import Scanner;
+
+public class Compiler implements SlxCompiler {
+    private Scanner scanner;
+    private Parser parser;
+
+    public static void main(String[] args) {
+        Compiler compiler = new Compiler();
+        SlxProgram program = compiler.compile(args[0]);
+
+        System.out.println("compiler.isErrors: " + (compiler.isErrors()));
+        if (program != null && !compiler.isErrors()) {
+            // Run the program
+        }
+        return;
+    }
+
+    public Compiler() {
+        return;
+    }
+
+    @Override
+    public boolean isErrors() {
+        return false;//this.parser.errors.count > 0;
+    }
+
+    @Override
+    public SlxProgram compile(String sourceFilename) {
+        this.scanner = new Scanner(sourceFilename);
+        this.parser = new Parser(this.scanner,  new Printer(false));
+        this.parser.Parse();
+        return null;
+    }
+}
