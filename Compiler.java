aaad6ee3fc8f100e836b352ae498013b1bb43dc2
Kristian Hartikainen <kristian@statwing.com>
2015-06-28 18:12:33 +0300

    Simplified the Compiler.java output file writing

--- Compiler.java
+++ Compiler.java
@@ -6,50 +6,7 @@ public class Compiler implements SlxCompiler {
     private Scanner scanner;
     private Parser parser;
 
-    public static void main(String[] args) {
-        String input_file_path = args[0];
-        Compiler compiler = new Compiler();
-        SlxProgram program = compiler.compile(input_file_path);
-        String output;
-        String filename = Compiler.parseFilename(input_file_path);
-
-        if (program != null && !compiler.isErrors()) {
-            // System.out.println("No errors found, running the program");
-            System.out.println("No errors found, writing the program to .slx file");
-
-            output = program.toString();
-            try {
-                System.out.println("trying");
-                PrintWriter writer = new PrintWriter("slx/" + input_file_path + ".slx", "UTF-8");
-                writer.print(output);
-                writer.close();
-            } catch (IOException ex) {
-                System.out.println("writer error");
-            }
-
-            // Run the program
-        } else {
-            System.out.println("Errors in the compiler");
-        }
-
-        return;
-    }
-
-    public Compiler() {
-    }
-
-    public static String parseFilename(String filepath) {
-        // e.g. filepath = "./easy/test/assignment.tst"
-        String[] parts;
-        String   filename;
-
-        parts = filepath.split("/"); // parts = [".", "easy", "test", "assignment.tst"]
-        filename = parts[parts.length - 1]; // filename = "assignment.tst"
-        parts = filename.split("\\."); // parts = ["assignment", "tst"]
-        filename = parts[0]; // filename = "assignment"
-
-        return filename;
-    }
+    public Compiler() {}
 
     @Override
     public boolean isErrors() {
@@ -57,12 +14,37 @@ public class Compiler implements SlxCompiler {
     }
 
     @Override
-    public SlxProgram compile(String sourceFilename) {
-        this.scanner = new Scanner(sourceFilename);
+    public SlxProgram compile(String source_file) {
+        this.scanner = new Scanner(source_file);
         this.parser = new Parser(this.scanner);
         this.parser.st = new SymbolTable(this.parser);
         this.parser.gen = new CodeGenerator();
         this.parser.Parse();
         return parser.gen.getProgram();
     }
+
+    public static void main(String[] args) {
+        String input_file_path = args[0];
+        Compiler compiler = new Compiler();
+        SlxProgram program = compiler.compile(input_file_path);
+        String output;
+
+        if (program != null && !compiler.isErrors()) {
+            System.out.println("No errors found, writing the program to .slx file");
+
+            output = program.toString();
+            try {
+                PrintWriter writer = new PrintWriter("out.slx", "UTF-8");
+                writer.print(output);
+                writer.close();
+            } catch (IOException ex) {
+                System.out.println("writer error");
+            }
+        } else {
+            System.out.println("Errors in the compiler");
+        }
+
+        return;
+    }
+
 }
